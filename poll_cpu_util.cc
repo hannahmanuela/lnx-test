fdb4f9d6454242f103e64bdf6fb10132addcc491
hmng <hmng@node-0.hmng-203968.mit-aifm-pg0.clemson.cloudlab.us>
2024-04-26 09:52:37 -0400

    runtimes based, initial

--- poll_cpu_util.cc
+++ poll_cpu_util.cc
@@ -22,95 +22,32 @@
 using namespace std;
 using namespace std::chrono;
 
+// can also include #include <linux/sched/types.h>, but then we get known include errors with sched_param being double defined
+struct sched_attr {
+    uint32_t size;
+
+    uint32_t sched_policy;
+    uint64_t sched_flags;
+
+    /* SCHED_NORMAL, SCHED_BATCH */
+    int32_t sched_nice;
+
+    /* SCHED_FIFO, SCHED_RR */
+    uint32_t sched_priority;
+
+    /* SCHED_DEADLINE (nsec) */
+    uint64_t sched_runtime;
+    uint64_t sched_deadline;
+    uint64_t sched_period;
+};
+
+#define NSEC_PER_MSEC 1000000
+
 high_resolution_clock::time_point global_start;
 
 #define NUM_PROCS_TO_MAKE 3
 #define NUM_LOW_PRIO 1
 #define NUM_MIDDLE_PRIO 1
-#define TO_FACTOR_BIIG 9369535334532
-#define TO_FACTOR_500 93695353
-#define TO_FACTOR_50  9768565
-#define TO_FACTOR_5   964653
-
-double timeDiff() {
-    duration<double> time_span = duration_cast<duration<double>>(high_resolution_clock::now() - global_start);
-    return 1000000 * time_span.count();  // 1000 => shows millisec; 1000000 => shows microsec
-}
-
-void readValFromLatencyFile() {
-    ifstream file("/sys/kernel/debug/sched/latency_ns");
-    if (file.is_open())
-        std::cout << file.rdbuf();
-}
-
-void writeValToLatencyFile(int val) {
-    int fd = open("/sys/kernel/debug/sched/latency_ns", O_RDWR);
-    if(fd == -1) {
-        cout << "open failed: " << strerror(errno) << endl;
-    }
-    string to_write = to_string(val);
-    size_t nb = write(fd, to_write.c_str(), to_write.size());
-    if (nb == -1) {
-        perror("Error writing");
-    }
-}
-
-// The function we want to execute on each thread.
-void comp_intense(int curr_latency) {
-
-    ofstream file;
-    file.open("../worker_out.txt", ios::app);
-    // file << timeDiff() << ", " << curr_latency << ", 1" << endl;
-    ofstream trash_file;
-    trash_file.open("../trash.txt", ios::app);
-
-    high_resolution_clock::time_point beg = high_resolution_clock::now();
- 
-    // ============================
-    // do the thing
-    // ============================
-    for (int i = 1; i <= TO_FACTOR_5; i++) 
-        if (TO_FACTOR_5 % i == 0) 
-            trash_file << " " << i << endl; 
-
-    high_resolution_clock::time_point end = high_resolution_clock::now();
-
-    duration<double> time_span = duration_cast<duration<double>>(end - beg);
-
-    file << timeDiff() << ", " <<  curr_latency  << ", " 
-        << 1000 * time_span.count() << endl; // 1000 => shows millisec; 1000000 => shows microsec
-    file.close();
-}
-
-void emptyFiles() {
-
-    // empty files we write to
-    ofstream polling_file;
-    polling_file.open("../polling_info.txt");
-    polling_file << "";
-    polling_file.close();
-
-    ofstream worker_file;
-    worker_file.open("../worker_out.txt");
-    worker_file << "";
-    worker_file.close();
-
-    ofstream proc_file;
-    proc_file.open("../proc_files.txt");
-    proc_file << "";
-    proc_file.close();
-
-    ofstream trash_file;
-    trash_file.open("../trash.txt");
-    trash_file << "";
-    trash_file.close();
-
-    ofstream times_file;
-    times_file.open("../times.txt");
-    times_file << "";
-    times_file.close();
-
-}
 
 int long_fac() {
     
@@ -155,8 +92,6 @@ int short_fac() {
 
 int main() {
 
-    emptyFiles();
-
     cpu_set_t  mask;
     CPU_ZERO(&mask);
     CPU_SET(0, &mask);
@@ -164,51 +99,46 @@ int main() {
         cout << "set affinity had an error" << endl;
     }
 
-    int fd_high_prio = open("/sys/fs/cgroup/one-digit-ms", O_RDONLY);
-    if(fd_high_prio == -1) {
-        cout << "open failed: " << strerror(errno) << endl;
-    }
-    int fd_mid_prio = open("/sys/fs/cgroup/two-digit-ms", O_RDONLY);
-    if(fd_mid_prio == -1) {
-        cout << "open failed: " << strerror(errno) << endl;
-    }
-    int fd_low_prio = open("/sys/fs/cgroup/three-digit-ms", O_RDONLY);
-    if(fd_low_prio == -1) {
-        cout << "open failed: " << strerror(errno) << endl;
-    }
+    int rt_high_prio = 5;
+    int rt_mid_prio = 50;
+    int rt_low_prio = 100;
 
     int c_pid;
     vector<int> pids;
 
     for(int i=0; i<NUM_PROCS_TO_MAKE; i++) {
-        int fd_to_use;
+        int rt_to_use;
         string prio;
         if (i < NUM_LOW_PRIO) {
-            fd_to_use = fd_low_prio;
+            rt_to_use = rt_low_prio;
             prio = "low";
         } else if (i < NUM_LOW_PRIO + NUM_MIDDLE_PRIO) {
-            fd_to_use = fd_mid_prio;
+            rt_to_use = rt_mid_prio;
             prio = "middle";
         } else {
-            fd_to_use = fd_high_prio;
+            rt_to_use = rt_high_prio;
             prio = "high";
         }
 
-        struct clone_args args = {
-            .flags = CLONE_INTO_CGROUP,
-            .exit_signal = SIGCHLD,
-            .cgroup = fd_to_use,
-            };
-        c_pid = syscall(SYS_clone3, &args, sizeof(struct clone_args));
+        c_pid = fork();
 
         if (c_pid == -1) { 
-            cout << "clone3 failed: " << strerror(errno) << endl;
-            perror("clone3"); 
+            cout << "fork failed: " << strerror(errno) << endl;
+            perror("fork"); 
             exit(EXIT_FAILURE); 
         } else if (c_pid > 0) {
             pids.push_back(c_pid);
             continue; 
         } else {
+
+        struct sched_attr attr;
+        syscall(SYS_sched_getattr, getpid(), &attr, 0);
+        attr.sched_runtime = rt_to_use * NSEC_PER_MSEC;
+        int ret = syscall(SYS_sched_setattr, getpid(), &attr, 0);
+        if (ret < 0) {
+            perror("sched_setattr");
+        }
+        cout << "set sched_runtime to be " << rt_to_use << endl;
             // set child affinity - they will both run on cpu 0
             cpu_set_t  mask;
             CPU_ZERO(&mask);
