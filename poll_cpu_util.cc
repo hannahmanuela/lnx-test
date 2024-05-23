211b833a46a7199b85b9c2a9472d7ff929bffbfb
hmng <hmng@node-0.hmng-206964.mit-aifm-pg0.clemson.cloudlab.us>
2024-05-23 10:21:07 -0400

    some testing changes, also includes important bug fix!

--- poll_cpu_util.cc
+++ poll_cpu_util.cc
@@ -45,52 +45,92 @@ struct sched_attr {
 
 high_resolution_clock::time_point global_start;
 
-#define NUM_PROCS_TO_MAKE 3
-#define NUM_LOW_PRIO 1
+#define NUM_PROCS_TO_MAKE 2
+#define NUM_LOW_PRIO 0
 #define NUM_MIDDLE_PRIO 1
 
+int time_since_start(high_resolution_clock::time_point start) {
+    std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - start);
+    return 1000 * time_span.count();  // 1000 => shows millisec; 1000000 => shows microsec
+}
+
+// 330 ms
 int long_fac() {
     
-    int to_factor = 43695353;
-    std::vector<int> factors;
+    // int to_factor = 243695353;
+    // std::vector<int> factors;
 
-    for (int i = 1; i <= to_factor; i++) {
-        if (to_factor % i == 0) {
-            factors.push_back(i);
-        }
+    // for (int i = 1; i <= to_factor; i++) {
+    //     if (to_factor % i == 0) {
+    //         factors.push_back(i);
+    //     }
+    // }
+
+    high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+    long long sum = 0;
+    for (long long i = 0; i < 100000000; i++) {
+        sum = 3 * i + 1;
     }
 
-    return 0;
+    cout << "long run: " << time_since_start(start) << "ms" << endl;
+
+    return sum;
 }
 
+// 170 ms
 int mid_fac() {
-    int to_factor = 7264565;
-    std::vector<int> factors;
+    // int to_factor = 67264565;
+    // std::vector<int> factors;
 
-    for (int i = 1; i <= to_factor; i++) {
-        if (to_factor % i == 0) {
-            factors.push_back(i);
-        }
+    // for (int i = 1; i <= to_factor; i++) {
+    //     if (to_factor % i == 0) {
+    //         factors.push_back(i);
+    //     }
+    // }
+
+    // return 0;
+
+    high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    long long sum = 0;
+    for (long long i = 0; i < 50000000; i++) {
+        sum = 3 * i + 1;
     }
 
-    return 0;
+    cout << "mid run: " << time_since_start(start) << "ms" << endl;
+
+    return sum;
 }
 
+// 50 ms
 int short_fac() {
-    int to_factor = 968253;
-    std::vector<int> factors;
+    // int to_factor = 7968253;
+    // std::vector<int> factors;
 
-    for (int i = 1; i <= to_factor; i++) {
-        if (to_factor % i == 0) {
-            factors.push_back(i);
-        }
+    // for (int i = 1; i <= to_factor; i++) {
+    //     if (to_factor % i == 0) {
+    //         factors.push_back(i);
+    //     }
+    // }
+
+    // return 0;
+    high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    long long sum = 0;
+    for (long long i = 0; i < 10000000; i++) {
+        sum = 3 * i + 1;
     }
 
-    return 0;
+    cout << "short run: " << time_since_start(start) << "ms" << endl;
+
+    return sum;
 }
 
 
 int main() {
+
+    cout << "parent pid: " << getpid() << endl;
+
+    global_start = std::chrono::high_resolution_clock::now();
 
     cpu_set_t  mask;
     CPU_ZERO(&mask);
@@ -99,8 +139,8 @@ int main() {
         cout << "set affinity had an error" << endl;
     }
 
-    int rt_high_prio = 3;
-    int rt_mid_prio = 30;
+    int rt_high_prio = 10;
+    int rt_mid_prio = 80;
     int rt_low_prio = 75;
 
     int c_pid;
@@ -131,14 +171,20 @@ int main() {
             continue; 
         } else {
 
-        struct sched_attr attr;
-        syscall(SYS_sched_getattr, getpid(), &attr, 0);
-        attr.sched_runtime = rt_to_use * NSEC_PER_MSEC;
-        int ret = syscall(SYS_sched_setattr, getpid(), &attr, 0);
-        if (ret < 0) {
-            perror("sched_setattr");
-        }
-        cout << "set sched_runtime to be " << rt_to_use << endl;
+            cout << "proc w/ prio " << prio << " starting after " << time_since_start(global_start) << endl;
+
+            struct sched_attr attr;
+            int ret = syscall(SYS_sched_getattr, getpid(), &attr, sizeof(attr), 0);;
+            if (ret < 0) {
+                perror("ERROR: sched_getattr");
+            }
+            attr.sched_runtime = rt_to_use * NSEC_PER_MSEC;
+            ret = syscall(SYS_sched_setattr, getpid(), &attr, 0);
+            if (ret < 0) {
+                perror("ERROR: sched_setattr");
+            }
+
+            // cout << "set sched_runtime to be " << rt_to_use << endl;
             // set child affinity - they will both run on cpu 0
             cpu_set_t  mask;
             CPU_ZERO(&mask);
@@ -146,7 +192,9 @@ int main() {
             if ( sched_setaffinity(0, sizeof(mask), &mask) > 0) {
                 cout << "set affinity had an error" << endl;
             }
+
             cout << "child w/ prio " << prio << " and pid " << getpid() << endl;
+
             if (prio == "low") {
                 long_fac();
             } else if (prio == "middle") {
@@ -154,12 +202,22 @@ int main() {
             } else {
                 short_fac();
             }
+
+            cout << "proc w/ prio " << prio << " ending after " << time_since_start(global_start) << endl;
+
             return 0;
-        } 
+            } 
     }
 
     for (int pid : pids) {
-        waitpid(pid, NULL, 0);
+        struct rusage usage_stats;
+        int ret = wait4(pid, NULL, 0, &usage_stats);
+        if (ret < 0) {
+            perror("wait4 did bad");
+        }
+        float runtime = (usage_stats.ru_utime.tv_sec * 1000.0 + (usage_stats.ru_utime.tv_usec/1000.0))
+                                + (usage_stats.ru_stime.tv_sec * 1000.0 + (usage_stats.ru_stime.tv_usec/1000.0));
+        cout << "rusage runtime for pid " << pid << " was " << runtime << endl;
     }
 
 }
