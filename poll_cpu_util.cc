d420160b75a4143b3daa086d89b5ca52035c0fa8
hannahmanuela <hannahmngross@gmail.com>
2024-10-16 15:52:51 -0400

    simple

--- poll_cpu_util.cc
+++ poll_cpu_util.cc
@@ -176,87 +176,108 @@ int main() {
         cout << "set affinity had an error" << endl;
     }
 
-    int rt_high_prio = 50;
-    int rt_mid_prio = 170;
-    int rt_low_prio = 400;
+    // int rt_high_prio = 50;
+    // int rt_mid_prio = 170;
+    // int rt_low_prio = 400;
 
-    int c_pid;
-    vector<int> pids;
+    // int c_pid;
+    // vector<int> pids;
 
-    for(int i=0; i<NUM_PROCS_TO_MAKE; i++) {
-        int rt_to_use;
-        string prio;
-        if (i < NUM_LOW_PRIO) {
-            rt_to_use = rt_low_prio;
-            prio = "low";
-        } else if (i < NUM_LOW_PRIO + NUM_MIDDLE_PRIO) {
-            rt_to_use = rt_mid_prio;
-            prio = "middle";
-        } else {
-            rt_to_use = rt_high_prio;
-            prio = "high";
-        }
+    // for(int i=0; i<NUM_PROCS_TO_MAKE; i++) {
+    //     int rt_to_use;
+    //     string prio;
+    //     if (i < NUM_LOW_PRIO) {
+    //         rt_to_use = rt_low_prio;
+    //         prio = "low";
+    //     } else if (i < NUM_LOW_PRIO + NUM_MIDDLE_PRIO) {
+    //         rt_to_use = rt_mid_prio;
+    //         prio = "middle";
+    //     } else {
+    //         rt_to_use = rt_high_prio;
+    //         prio = "high";
+    //     }
 
-        c_pid = fork();
+    //     c_pid = fork();
 
-        if (c_pid == -1) { 
-            cout << "fork failed: " << strerror(errno) << endl;
-            perror("fork"); 
-            exit(EXIT_FAILURE); 
-        } else if (c_pid > 0) {
-            pids.push_back(c_pid);
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
-            continue; 
-        } else {
+    //     if (c_pid == -1) { 
+    //         cout << "fork failed: " << strerror(errno) << endl;
+    //         perror("fork"); 
+    //         exit(EXIT_FAILURE); 
+    //     } else if (c_pid > 0) {
+    //         pids.push_back(c_pid);
+    //         std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    //         continue; 
+    //     } else {
 
-            cout << "proc w/ prio " << prio << " starting after " << time_since_start(global_start) << endl;
+    //         cout << "proc w/ prio " << prio << " starting after " << time_since_start(global_start) << endl;
 
-            struct sched_attr attr;
-            int ret = syscall(SYS_sched_getattr, getpid(), &attr, sizeof(attr), 0);
-            if (ret < 0) {
-                perror("ERROR: sched_getattr");
-            }
-            attr.sched_runtime = rt_to_use * NSEC_PER_MSEC;
-            ret = syscall(SYS_sched_setattr, getpid(), &attr, 0);
-            if (ret < 0) {
-                perror("ERROR: sched_setattr");
-            }
+    struct sched_attr attr;
+    int ret = syscall(SYS_sched_getattr, getpid(), &attr, sizeof(attr), 0);
+    if (ret < 0) {
+        perror("ERROR: sched_getattr");
+    }
+    attr.sched_runtime = 50 * NSEC_PER_MSEC;
+    ret = syscall(SYS_sched_setattr, getpid(), &attr, 0);
+    if (ret < 0) {
+        perror("ERROR: sched_setattr");
+    }
+
+
+    high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+    long long sum = 0;
+    for (long long i = 0; i < 10000000; i++) {
+        sum = 3 * i + 1;
+    }
+
+    cout << "took: " << time_since_start(start) << "ms" << endl;
+
+    // struct sched_attr attr;
+    ret = syscall(SYS_sched_getattr, getpid(), &attr, sizeof(attr), 0);
+    if (ret < 0) {
+        perror("ERROR: sched_getattr");
+    }
+    attr.sched_runtime = 8 * NSEC_PER_MSEC;
+    ret = syscall(SYS_sched_setattr, getpid(), &attr, 0);
+    if (ret < 0) {
+        perror("ERROR: sched_setattr");
+    }
 
             // cout << "set sched_runtime to be " << rt_to_use << endl;
             // set child affinity - they will both run on cpu 0
-            cpu_set_t  mask;
-            CPU_ZERO(&mask);
-            CPU_SET(1, &mask);
-            if ( sched_setaffinity(0, sizeof(mask), &mask) > 0) {
-                cout << "set affinity had an error" << endl;
-            }
+    //         cpu_set_t  mask;
+    //         CPU_ZERO(&mask);
+    //         CPU_SET(1, &mask);
+    //         if ( sched_setaffinity(0, sizeof(mask), &mask) > 0) {
+    //             cout << "set affinity had an error" << endl;
+    //         }
 
-            cout << "child w/ prio " << prio << " and pid " << getpid() << ", now at " << time_since_start(global_start) << endl;
+    //         cout << "child w/ prio " << prio << " and pid " << getpid() << ", now at " << time_since_start(global_start) << endl;
 
-            if (prio == "low") {
-                long_fac();
-            } else if (prio == "middle") {
-                mid_fac();
-            } else {
-                short_fac();
-            }
+    //         if (prio == "low") {
+    //             long_fac();
+    //         } else if (prio == "middle") {
+    //             mid_fac();
+    //         } else {
+    //             short_fac();
+    //         }
 
-            cout << "proc w/ prio " << prio << " ending after " << time_since_start(global_start) << endl;
+    //         cout << "proc w/ prio " << prio << " ending after " << time_since_start(global_start) << endl;
 
-            return 0;
-            } 
-    }
+    //         return 0;
+    //         } 
+    // }
 
-    for (int pid : pids) {
-        struct rusage usage_stats;
-        int ret = wait4(pid, NULL, 0, &usage_stats);
-        if (ret < 0) {
-            perror("wait4 did bad");
-        }
-        float runtime = (usage_stats.ru_utime.tv_sec * 1000.0 + (usage_stats.ru_utime.tv_usec/1000.0))
-                                + (usage_stats.ru_stime.tv_sec * 1000.0 + (usage_stats.ru_stime.tv_usec/1000.0));
-        cout << "rusage runtime for pid " << pid << " was " << runtime << endl;
-    }
+    // for (int pid : pids) {
+    //     struct rusage usage_stats;
+    //     int ret = wait4(pid, NULL, 0, &usage_stats);
+    //     if (ret < 0) {
+    //         perror("wait4 did bad");
+    //     }
+    //     float runtime = (usage_stats.ru_utime.tv_sec * 1000.0 + (usage_stats.ru_utime.tv_usec/1000.0))
+    //                             + (usage_stats.ru_stime.tv_sec * 1000.0 + (usage_stats.ru_stime.tv_usec/1000.0));
+    //     cout << "rusage runtime for pid " << pid << " was " << runtime << endl;
+    // }
 
 }
 
